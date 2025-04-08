946bcd30ead9eee86b3a0b02e282f3a064a65f0c
tourze-ci <you@example.com>
2025-04-08 16:29:08 +0000

    refactor: 更新依赖和代码改进

--- src/YunpianSmsBundle.php
+++ src/YunpianSmsBundle.php
@@ -3,17 +3,7 @@
 namespace YunpianSmsBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Tourze\BundleDependency\BundleDependencyInterface;
-use Tourze\EasyAdmin\Attribute\Permission\AsPermission;
 
-#[AsPermission(title: '云片短信')]
-class YunpianSmsBundle extends Bundle implements BundleDependencyInterface
+class YunpianSmsBundle extends Bundle
 {
-    public static function getBundleDependencies(): array
-    {
-        return [
-            \AmisBundle\AmisBundle::class => ['all' => true],
-            \Tourze\Symfony\CronJob\CronJobBundle::class => ['all' => true],
-        ];
-    }
 }
